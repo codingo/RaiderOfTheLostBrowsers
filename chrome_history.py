0b947ba58d48ac833377ee3c683ed1389fe1305c
Michael Skelton <michael@codingo.com.au>
2017-04-15 11:55:07 +1000

    Create chrome_history.py

---
+++ chrome_history.py
@@ -0,0 +1,43 @@
+import os
+import sqlite3
+import operator
+from collections import OrderedDict
+
+#website to analyse
+syte="yahoo.com"
+
+
+def parse(url):
+    try:
+        parsed_url_components = url.split('//')
+        sublevel_split = parsed_url_components[1].split('/', 1)
+        domain = sublevel_split[0].replace("www.", "")
+        return domain
+    except:
+        False
+
+    
+#path to user's history database (Chrome)
+data_path = os.path.expanduser('~')+"\AppData\Local\Google\Chrome\User Data\Default"
+files = os.listdir(data_path)
+
+history_db = os.path.join(data_path, 'history')
+
+#querying the db
+c = sqlite3.connect(history_db)
+cursor = c.cursor()
+select_statement = "SELECT urls.url, urls.visit_count FROM urls, visits WHERE urls.id = visits.url;"
+cursor.execute(select_statement)
+
+results = cursor.fetchall() #tuple
+
+sites_count = {} #dict makes iterations easier :D
+fl=[]
+for url, count in results:
+    url = parse(url)
+    if syte in str(url):
+        #print url
+        if url not in fl:
+            fl.append(url)
+print "Results:"
+print fl
