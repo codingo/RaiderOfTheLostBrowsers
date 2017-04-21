f5218999e1c0a89a3c7ec12dd6873aaef371b411
Michael <codingo@outlook.com>
2017-04-21 14:41:40 +1000

    Adding command line arguments and refactoring

--- chrome_history.py
+++ chrome_history.py
@@ -1,11 +1,12 @@
 import os
 import sqlite3
 import operator
+import sys
 from collections import OrderedDict
+from argparse import ArgumentParser
 
-# website to analyse
-site="reddit"
 
+VERSION = '0.1 UNSTABLE'
 
 def parse(url):
     try:
@@ -16,31 +17,43 @@ def parse(url):
     except:
         False
     
-# path to user's history database (Chrome)
-data_path = os.path.expanduser('~') + str("\\AppData\\Local\\Google\\Chrome\\User Data\\Default")
-files = os.listdir(data_path)
+def query_database(search_term):
+    # path to user's history database (Chrome)
+    data_path = os.path.expanduser('~') + str("\\AppData\\Local\\Google\\Chrome\\User Data\\Default")
+    files = os.listdir(data_path)
+    history_db = os.path.join(data_path, 'history')
 
-history_db = os.path.join(data_path, 'history')
-
-# querying the db
-c = sqlite3.connect(history_db)
-cursor = c.cursor()
-select_statement = """  SELECT DISTINCT urls.url, urls.visit_count 
+    # querying the db
+    c = sqlite3.connect(history_db)
+    cursor = c.cursor()
+    statement = """     SELECT DISTINCT urls.url, count(urls.visit_count) 
                         FROM            urls, visits 
                         WHERE           urls.id = visits.url 
-                        GROUP BY        urls.url, urls.visit_count;"""
-cursor.execute(select_statement)
+                        GROUP BY        urls.url;"""
+    cursor.execute(statement)
 
-results = cursor.fetchall()
+    results = cursor.fetchall()
 
-sites_count = {}
-fl=[]
-for url, count in results:
-    url = parse(url)
-    # print(url)
-    if site in str(url):
-        # print url
-        if url not in fl:
-            fl.append(url)
-print("Results:")
-print(fl)
+    sites_count = {}
+    fl=[]
+    for url, count in results:
+        url = parse(url)
+        if search_term in str(url):
+            # print url
+            if url not in fl:
+                fl.append(url)
+    print("Results:")
+    print(fl)
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("-term", dest="term", help="Provide a term to search for in URL's. Ex: https://test.sharepoint.com")
+    arguments = parser.parse_args()
+
+    if len(sys.argv) == 1:
+        parser.error("No arguments given. Displaying all websites available.")
+
+    query_database(arguments.term)
+
+if __name__ == "__main__":
+    main()
